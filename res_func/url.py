95cfd0ff146ae0ef144ff0dce5fc23c6a1d87cec
xtaodada <xtao@xtaolink.cn>
2023-07-20 11:33:36 +0800

    🐛 fix: parse starraildata

--- res_func/url.py
+++ res_func/url.py
@@ -4,7 +4,7 @@ base_data_url = base64.b64decode(
     "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0RpbWJyZWF0aC9TdGFyUmFpbERhdGEvbWFzdGVyLw=="
 ).decode("utf-8")
 avatar_config = f"{base_data_url}ExcelOutput/AvatarConfig.json"
-text_map = f"{base_data_url}TextMap/TextMapCN.json"
+text_map = f"{base_data_url}TextMap/TextMapCHS.json"
 relic_config = f"{base_data_url}ExcelOutput/RelicConfig.json"
 relic_main_affix_config = f"{base_data_url}ExcelOutput/RelicMainAffixConfig.json"
 relic_sub_affix_config = f"{base_data_url}ExcelOutput/RelicSubAffixConfig.json"
